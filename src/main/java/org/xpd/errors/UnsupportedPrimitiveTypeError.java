77ad0afa8c503eae9529acaa9b8a8e6112834013
pedro <peidong.xu94@gmail.com>
2026-07-02 16:35:17 +1000

    delete unused Error

--- src/main/java/org/xpd/errors/UnsupportedPrimitiveTypeError.java
+++
@@ -1,7 +0,0 @@
-package org.xpd.errors;
-
-public class UnsupportedPrimitiveTypeError extends RuntimeException {
-    public UnsupportedPrimitiveTypeError(String target) {
-        super(String.format("unsupported primitive type: %s", target));
-    }
-}
